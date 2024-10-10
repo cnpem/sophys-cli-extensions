5b4dcbf856d0e13c89ffbe39154b04ecdf42b2c7
Sofia Donato Ferreira <sofia.ferreira@lnls.br>
2024-10-10 17:53:03 -0300

    feat(EMA): add 1D scans
    Signed-off-by: Sofia Donato Ferreira <sofia.ferreira@lnls.br>

--- src/sophys/cli/extensions/ema/__init__.py
+++ src/sophys/cli/extensions/ema/__init__.py
@@ -6,8 +6,46 @@ from ..plan_magics import get_plans, ModeOfOperation, PlanInformation, PlanWhite
 from ..tools_magics import KBLMagics, HTTPMagics, MiscMagics
 
 from ..plan_magics import PlanMV, PlanCount, PlanScan, PlanGridScan, PlanAdaptiveScan
+from ..plan_magics import PlanCLI, BPlan
 
 from .input_processor import LocalDataSource, input_processor
+
+
+class Plan1DScan(PlanCLI):
+    absolute: bool
+
+    def create_parser(self):
+        _a = super().create_parser()
+
+        _a.add_argument("motor", nargs=1, type=str)
+        _a.add_argument("start", type=float)
+        _a.add_argument("stop", type=float)
+        _a.add_argument("num", type=int)
+        _a.add_argument("exposure_time", type=float, nargs='?', default=None)
+
+        return _a
+
+    def _create_plan(self, parsed_namespace, local_ns):
+        detector = self.get_real_devices_if_needed(parsed_namespace.detectors, local_ns)
+        motor = self.get_real_devices_if_needed(parsed_namespace.motor, local_ns)[0]
+        start = parsed_namespace.start
+        stop = parsed_namespace.stop
+        num = parsed_namespace.num
+        exp_time = parsed_namespace.exposure_time
+        md = self.parse_md(parsed_namespace)
+
+        if self._mode_of_operation == ModeOfOperation.Local:
+            return functools.partial(self._plan, detector, motor, start, stop, num, exp_time, md=md, absolute=self.absolute)
+        if self._mode_of_operation == ModeOfOperation.Remote:
+            return BPlan(self._plan_name, detector, motor, start, stop, num, exp_time, md=md, absolute=self.absolute)
+
+
+class PlanAbs1DScan(Plan1DScan):
+    absolute = True
+
+
+class PlanRel1DScan(Plan1DScan):
+    absolute = False
 
 
 PLAN_WHITELIST = PlanWhitelist([
@@ -16,6 +54,8 @@ PLAN_WHITELIST = PlanWhitelist([
     PlanInformation("scan", "scan", PlanScan),
     PlanInformation("grid_scan", "grid_scan", PlanGridScan),
     PlanInformation("adaptive_scan", "adaptive_scan", PlanAdaptiveScan),
+    PlanInformation("scan1d", "ascan", PlanAbs1DScan),
+    PlanInformation("scan1d", "rscan", PlanRel1DScan),
 ])
 
 
